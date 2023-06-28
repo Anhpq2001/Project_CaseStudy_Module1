efd81fce70198bb3dde6540fa6ce76ea5b13a990
Phạm Quang Anh <106795680+Anhpq2001@users.noreply.github.com>
2023-06-29 02:15:29 +0700

    update LogIn.js file

--- Javascript/LogIn.js
+++ Javascript/LogIn.js
@@ -1,6 +1,13 @@
-let username = document.getElementById("username").value;
-let password = document.getElementById("password").value;
+let username = document.getElementById("username");
+let password = document.getElementById("password");
 // ham check validate
 function checkValue(){
-
+    let data = JSON.parse(localStorage.getItem("USER_INFO"));
+    console.log(data.phoneNumber)
+    console.log(data.password)
+    if(data.phoneNumber === username.value && data.password === password.value){
+        window.location.replace("../Html/index.html");
+    }else {
+        alert("Password incorrect!");
+    }
 }
