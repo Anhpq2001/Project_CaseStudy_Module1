381facb56fec6cb5a082f921027796811c7b91ea
Phạm Quang Anh <106795680+Anhpq2001@users.noreply.github.com>
2023-06-29 01:18:12 +0700

    update file

--- Javascript/SignIn.js
+++ Javascript/SignIn.js
@@ -1,52 +1,91 @@
 // get value from textbox
-let usernameValue = document.getElementById("usernameId").value;
-let phoneNumberValue = document.getElementById("phoneNumberId").value;
-let passwordValue = document.getElementById("passwordId").value;
-let passwordConfirmValue = document.getElementById("passwordConfirmId").value;
-let form = document.querySelector("form");
-// function to show error
+let usernameValue = document.getElementById("usernameId");
+let phoneNumberValue = document.getElementById("phoneNumberId");
+let passwordValue = document.getElementById("passwordId");
+let passwordConfirmValue = document.getElementById("passwordConfirmId");
+var form = document.querySelector("form");
+// class User
+class User{
+    name;
+    phoneNumber;
+    password;
+    constructor(name, phoneNumber, password) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.password = password;
+    }
+}
+//  function check error
 function showError(input, message){
-    //let parent = input.parentElement;
-    let small = document.querySelector("small");
-    small.classList.add("error");
+    let parent = input.parentElement;
+    let small = parent.querySelector("small");
+    parent.classList.add("error");
     small.innerText = message;
 }
-// function to show successfull
-function showSuccessfull(input){
-    //let parent = input.parentElement;
-    let small = document.querySelector("small");
-    small.classList.remove("error");
+// function check successful
+function showSuccessful(input){
+    let parent = input.parentElement;
+    let small = parent.querySelector("small");
+    parent.classList.remove("error");
     small.innerText = "";
 }
 // function check empty
-function checkEmpty(listInput){
-    let isEmptyValue =  false;
-    listInput.forEach(input =>{
-        input = input.value;
-        if(!input){
-            showError(input,"Not empty!");
-            isEmptyValue = true;
+function checkEmptyError(listInput){
+    let isEmptyError = false;
+    listInput.forEach(input => {
+       input.value = input.value.trim();
+        if(!input.value){
+            isEmptyError = true;
+            showError(input, "Not Empty");
         }else{
-            showSuccessfull(input);
+            showSuccessful(input);
         }
     });
-    return isEmptyValue;
+    return isEmptyError;
 }
-// function check length of value
-function checkLength(){
-
-}
-// function compare password with passwordConfirm
-function checkPasswordConfirm(password, passwordConfirm){
-    let isEqual = false;
-    if(password === passwordConfirm){
-        isEqual =  true;
-    }else{
-        showError(passwordConfirm, "Not fit with password")
+// function check digit
+function checkDigit(input, min, max){
+    input.value = input.value.trim();
+    if(input.value.length <= min){
+        showError(input, "Must have 10 digits");
+        return true;
     }
+    else if(input.value.length >= max){
+        showError(input, "Must have 10 digits");
+        return true;
+    }
+
+    showSuccessful(input);
+
+    return false;
 }
-// fuction to check even
-form.addEventListener("submit", function (e){
+// function check match password
+function checkMatchPassword(passwordInput, cfPasswordInput){
+    if(passwordInput.value !== cfPasswordInput.value){
+        showError(cfPasswordInput, "Confirm password not match");
+        return true;
+    }
+    return false;
+}
+// even
+form.addEventListener("submit", function(e){
     e.preventDefault();
-    checkPasswordConfirm(password, passwordConfirmValue);
+    let isEmptyError = checkEmptyError([usernameValue, phoneNumberValue, passwordValue, passwordConfirmValue]);
+    let checkDigits = checkDigit(phoneNumberValue, 9, 11);
+    let matchpassword = checkMatchPassword(passwordValue, passwordConfirmValue);
+    if(isEmptyError || checkDigits || matchpassword){
+        alert("Enter data error");
+    }else{
+        let user = new User();
+        user.name = usernameValue.value;
+        user.phoneNumber = phoneNumberValue.value;
+        user.password = passwordValue.value;
+        console.log(user);
+        localStorage.setItem("USER_INFO", JSON.stringify(user));
+        alert("SignIn Successfull!");
+    }
 })
+
+
+
+
