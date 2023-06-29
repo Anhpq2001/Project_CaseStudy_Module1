06f29d611d8f2805fab12dc18516d1dfa9b4ab79
Phạm Quang Anh <106795680+Anhpq2001@users.noreply.github.com>
2023-06-29 08:33:22 +0700

    update all

--- Javascript/Home.js
+++ Javascript/Home.js
@@ -11,11 +11,11 @@ class Product{
 
 let obj0 = new Product("../image/1.jpg", "Hoa Hồng Trắng", 200);
 let obj1 = new Product("../image/2.jpg", "Hoa Đồng Tiền", 200);
-let obj2 = new Product("../image/3.jpg", "Hoa Hồng Đỏ", 200);
-let obj3 = new Product("../image/3.jpg", "Hoa Hồng Đỏ", 200);
-let obj4 = new Product("../image/3.jpg", "Hoa Hồng Đỏ", 200);
-let obj5 = new Product("../image/3.jpg", "Hoa Hồng Đỏ", 200);
-let obj6 = new Product("../image/3.jpg", "Hoa Hồng Đỏ", 200);
+let obj2 = new Product("../image/3.jpg", "Hoa Hồng Đỏ", 100);
+let obj3 = new Product("../image/4.jpg", "Hoa Hướng Dương 1", 150);
+let obj4 = new Product("../image/5.jpg", "Hoa Hướng Dương 2", 200);
+let obj5 = new Product("../image/6.jpg", "Lẵng hoa hồng đỏ", 200);
+let obj6 = new Product("../image/7.jpg", "Hoa tuyết cẩm", 500);
 
 let listProduct = [obj0, obj1, obj2, obj3, obj4, obj5, obj6];
 // function to show list product in Home page
@@ -26,17 +26,16 @@ function showProductForHome(){
             + "<img src=" + listProduct[i].image + "><br>"
             + "<input type='submit' value='ADD TO CART'>"
             + "<p>" + listProduct[i].name + "</p>"
-            + "<p>" + listProduct[i].price +"</p>"
+            + "<p>" + listProduct[i].price +" USD</p>"
             + "</div></div>";
     }
-    document.getElementById("listProduct").innerHTML = str
+    document.getElementById("listProduct").innerHTML = str;
 }
 // function to add item to cart
 function addItemToCart(){
 
 }
 // function to search item in home page
-
 showProductForHome();
 
 
